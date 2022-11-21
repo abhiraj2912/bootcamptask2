7975b9263cf17c42ce7f2f24eada3d7d6f587c11
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-21 12:22:58 +0530

    first commit

---
+++ src/Menu2.java
@@ -0,0 +1,8 @@
+public class Menu2 {
+    public static void main(String[] args) {
+        int x= 1;
+        while (x==1){
+            System.out.println("1. Largest of 3 number");
+        }
+    }
+}
