cf7c2a830207cb3332769d35c4e3581ca24f385d
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-21 14:34:19 +0530

    primeornot

--- src/Menu2.java
+++ src/Menu2.java
@@ -7,12 +7,31 @@ public class Menu2 {
     static int smallestOfThree(int a, int b, int c){
         return ((a<b&&a<c)?a:((b<c)?b:c));
     }
+
+    static void primeOrNot(int a){
+        int f=1;
+        for(int i=2; i<=a/2; i++){
+            if(a%i==0){
+                f=2;
+                break;
+
+            }
+        }
+        if (f==2){
+            System.out.println("Not a prime number");
+
+        }else
+        {
+            System.out.println("Prime number");
+        }
+    }
     public static void main(String[] args) {
         int x= 1,n, a, b, c;
         while (x==1){
             System.out.println("Enter your choice");
             System.out.println("1. Largest of 3 number");
             System.out.println("2. Smallest of 3 number");
+            System.out.println("3. Prime or not");
             System.out.println("6. Exit");
 
             Scanner sc = new Scanner(System.in);
@@ -32,6 +51,11 @@ public class Menu2 {
                     c=sc.nextInt();
                     System.out.println("Smallest is "+smallestOfThree(a,b,c));
                     break;
+                case 3:
+                    System.out.println("Enter the number");
+                    a=sc.nextInt();
+                    primeOrNot(a);
+
 
                 case 6:
                     System.out.println("Thanks for using");
